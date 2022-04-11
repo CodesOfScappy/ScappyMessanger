690381accb64cb7970f92721de2b9a17063a3c63
CodeOfScappy <Info.scappy@gmail.com>
2022-04-11 14:47:51 +0200

    This App is not Finish!!!
    is a Copy to Workflow

---
+++ app/src/main/java/com/example/scappyapp/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.scappyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
---
+++ app/src/test/java/com/example/scappyapp/ActivitySignUpBinding.java
@@ -0,0 +1,4 @@
+package com.example.scappyapp;
+
+public class ActivitySignUpBinding {
+}
